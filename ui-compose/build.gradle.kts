bbc0bd19f44b541c4293bb641fd40ccd72af8871
Patrick Goldinger <patrick@patrickgold.dev>
2021-09-05 19:24:22 +0200

    Upgrade to Jetpack Compose 1.0.2

--- ui-compose/build.gradle.kts
+++ ui-compose/build.gradle.kts
@@ -46,9 +46,9 @@ android {
 dependencies {
     implementation("androidx.activity:activity-compose:1.3.1")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.compose.ui:ui:1.0.1")
-    implementation("androidx.compose.material:material:1.0.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.0.1")
+    implementation("androidx.compose.ui:ui:1.0.2")
+    implementation("androidx.compose.material:material:1.0.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.0.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.0.1")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
@@ -58,6 +58,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.0.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.0.2")
 }
