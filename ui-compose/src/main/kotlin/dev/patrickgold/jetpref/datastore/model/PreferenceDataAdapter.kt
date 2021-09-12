102dbf67a19608a796b39daa9397eb287abbcbc7
Patrick Goldinger <patrick@patrickgold.dev>
2021-09-12 23:59:11 +0200

    Change PreferenceDataAdapter so it exposes an immutable state

--- ui-compose/src/main/kotlin/dev/patrickgold/jetpref/datastore/model/PreferenceDataAdapter.kt
+++ ui-compose/src/main/kotlin/dev/patrickgold/jetpref/datastore/model/PreferenceDataAdapter.kt
@@ -26,16 +26,16 @@ package dev.patrickgold.jetpref.datastore.model
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalLifecycleOwner
 
 @Composable
-fun <V : Any> PreferenceData<V>.observeAsState(): MutableState<V> = observeAsState(get())
+fun <V : Any> PreferenceData<V>.observeAsState(): State<V> = observeAsState(get())
 
 @Composable
-fun <V : Any> PreferenceData<V>.observeAsState(initial: V): MutableState<V> {
+fun <V : Any> PreferenceData<V>.observeAsState(initial: V): State<V> {
     val lifecycleOwner = LocalLifecycleOwner.current
     val state = remember(key) { mutableStateOf(initial) }
     DisposableEffect(this, lifecycleOwner) {
