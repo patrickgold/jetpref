db4e0be119ed40c64e58c0151e30fbad9a7badfd
Patrick Goldinger <patrick@patrickgold.dev>
2021-12-16 22:05:39 +0100

    Upgrade to Jetpack Compose 1.1.0-rc01 / Gradle 7.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
     }
 }
