ca9f8f87b105dd458789f4841529437a2575a7db
Patrick Goldinger <patrick@patrickgold.dev>
2022-01-24 21:55:08 +0100

    Remove unnecessary dependencies in `material-ui`

--- material-ui/build.gradle.kts
+++ material-ui/build.gradle.kts
@@ -64,15 +64,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation(project(":datastore-model"))
 
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
