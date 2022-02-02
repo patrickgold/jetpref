579c17b1ec6f477355436bda95a4a2775d62c522
Patrick Goldinger <patrick@patrickgold.dev>
2022-02-03 00:17:40 +0100

    Update AGP to 7.1.0 and Gradle to 7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
     }
